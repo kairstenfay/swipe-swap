e135024879e93d9b84b97f7a9bc10d1d5e27f0c7
Kairsten Fay <kairsten.fay@gmail.com>
2019-08-06 14:06:44 -0700

    Add React front-end

---
+++ web/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="container">
+          <div className="row">
+            <div className="col-xs-2">
+              <img src={logo} className="App-logo" alt="logo" />
+            </div>
+            <div class="col-xs-6">
+              Bartering Made Easy - Swipe, Swap, Sweet.
+            </div>
+          </div>
+        </div>
+      </header>
+      <button type="button" class="btn btn-primary">Join</button>
+    </div>
+  );
+}
+
+export default App;
