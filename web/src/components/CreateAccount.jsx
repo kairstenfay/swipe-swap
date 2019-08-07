61b6cb1b13f3c1c05f55be0341490e83bd6f2799
Kairsten Fay <kairsten.fay@gmail.com>
2019-08-07 05:41:55 -0700

    Add Create Account fixiup

--- web/src/components/CreateAccount.jsx
+++ web/src/components/CreateAccount.jsx
@@ -1,26 +1,57 @@
 import React from 'react';
 import '../App.css';
 
-function CreateAccount() {
-  return (
-    <div className="container">
-        <form action="/success" target="_blank" method="post">
-            <label>
-                First name:
-                <input type="text" name="firstname" />
-            </label>
-            <label>
-                Last name:
-                <input type="text" name="lastname" />
-            </label>
-            <label>
-                Username:
-                <input type="text" name="username" />
-            </label>
-            <input type="submit" class="btn btn-primary" value="Join" />
-        </form>
-    </div>
-  );
+class CreateAccount extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {value: ''};
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    handleSubmit(event) {
+        fetch('https://localhost:5000/v1/create/individual', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                firstParam: 'yourValue',
+                secondParam: 'yourOtherValue',
+            })
+        })
+        alert('An account was created: ' + this.state.value);
+        event.preventDefault();
+    }
+
+    render () {
+        return (
+            <div className="container">
+                <form action="/success" onSubmit={this.handleSubmit}>
+                    <label>
+                        First name:
+                        <input type="text" name="firstname" value={this.state.value} onChange={this.handleChange}/>
+                    </label>
+                    <label>
+                        Last name:
+                        <input type="text" name="lastname" />
+                    </label>
+                    <label>
+                        Username:
+                        <input type="text" name="username" />
+                    </label>
+                    <br />
+                    <input type="submit" className="btn btn-primary" value="Join" />
+                </form>
+            </div>
+        );
+    }
 }
 
 export default CreateAccount;
